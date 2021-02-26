0be53260ac03507b9339977c002783b3893db858
micaelacolpi <micaelacolpi@yahoo.com>
2021-02-26 10:28:54 -0600

    moved java

--- main.js
+++
@@ -1,4 +0,0 @@
-console.log(hey girl)
-import {fishCollection} from "./scripts/FishData.js"
-
-console.log (fishCollection)
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+console.log(hey girl)
+import {fishCollection} from "./FishData.js"
+
+console.log (fishCollection)
